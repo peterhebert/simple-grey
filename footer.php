697e4a42999d133500f16b8a5d254fff0c24b421
Peter Hebert <peter@rexrana.ca>
2014-12-14 23:31:08 -0800

    updated footer references from 'peterhebert' to 'simple_grey'

--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
 
 	<footer class="site-footer" role="contentinfo">
 	  <div class="wrap">
-    <?php if ( get_theme_mod( 'peterhebert_footer_text' ) ) : ?>
+    <?php if ( get_theme_mod( 'simple_grey_footer_text' ) ) : ?>
     <div class="footer-credits">
-      <p><?php echo get_theme_mod( 'peterhebert_footer_text' ); ?></p>
+      <p><?php echo get_theme_mod( 'simple_grey_footer_text' ); ?></p>
 	  </div><!-- .footer-credits -->
     <?php endif; ?>
 
@@ -26,13 +26,13 @@
   <?php endif; ?>
 
     <div class="footer-credits">
-    <?php if ( get_theme_mod( 'peterhebert_copyright_info' ) ) : ?>
-      <p><?php echo get_theme_mod( 'peterhebert_copyright_info' ); ?></p>
+    <?php if ( get_theme_mod( 'simple_grey_copyright_info' ) ) : ?>
+      <p><?php echo get_theme_mod( 'simple_grey_copyright_info' ); ?></p>
     <?php endif; ?>
-    <?php if ( get_theme_mod( 'peterhebert_show_footer_credits' ) != '' ) : ?>
-	      <?php do_action( 'peterhebert_credits' ); ?>
+    <?php if ( get_theme_mod( 'simple_grey_show_footer_credits' ) != '' ) : ?>
+	      <?php do_action( 'simple_grey_credits' ); ?>
 	      <p><i class="mv mv-wordpress icon-large"></i> <a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'simple-grey' ), 'WordPress' ); ?></a>. 
-	      <?php printf( __( 'Theme: %1$s by %2$s.', 'simple-grey' ), sprintf( __( '<a href="%1$s">%2$s</a>', 'simple-grey' ), __( 'http://peterhebert.com/', 'simple-grey' ), __( 'Peter Hebert', 'simple-grey' ) ), sprintf( __( '<a href="%1$s" rel="designer">%2$s</a>', 'simple-grey' ), __( 'http://peterhebert.com/', 'simple-grey' ), __( 'Peter Hebert', 'simple-grey' ) ) ); ?></p>
+	      <?php printf( __( 'Theme: %1$s by %2$s.', 'simple-grey' ), sprintf( __( '<a href="%1$s">%2$s</a>', 'simple-grey' ), __( 'http://peterhebert.com/', 'simple-grey' ), __( 'Simple Grey', 'simple-grey' ) ), sprintf( __( '<a href="%1$s" rel="designer">%2$s</a>', 'simple-grey' ), __( 'http://peterhebert.com/', 'simple-grey' ), __( 'Peter Hebert', 'simple-grey' ) ) ); ?></p>
     <?php endif; ?>
 	  </div><!-- .footer-credits -->
     </div><!-- .wrap -->
