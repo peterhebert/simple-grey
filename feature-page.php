019bdaac3daf72e1fe9ad39a48cb36781d21e7fa
Peter Hebert <peter@peterhebert.com>
2017-07-09 12:26:07 -0700

    Added comments to page templates

--- feature-page.php
+++ feature-page.php
@@ -13,7 +13,13 @@ get_header(); ?>
       <?php if ( have_posts() ) : ?>
         <?php /* Start the Loop */ ?>
         <?php while ( have_posts() ) : the_post(); ?>
-          <?php get_template_part( 'content', 'page' ); ?>
+        	<?php get_template_part( 'content', 'page' ); ?>
+          <?php
+          // If comments are open or we have at least one comment, load up the comment template
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
+          endif;
+          ?>
         <?php endwhile; ?>
         <?php else : ?>
         <?php get_template_part( 'content', 'none' ); ?>
--- page.php
+++ page.php
@@ -13,21 +13,21 @@
 get_header(); ?>
 
     <main id="main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
+      <?php if ( have_posts() ) : ?>
+        <?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+        	<?php get_template_part( 'content', 'page' ); ?>
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			?>
+        <?php endwhile; ?>
+        <?php else : ?>
+        <?php get_template_part( 'content', 'none' ); ?>
+        <?php endif; ?>
+      </main>
       <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
