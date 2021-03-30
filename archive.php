de00d9fde9281c87c94530d828f4495d5946f9dc
Peter Hebert <peter@peterhebert.com>
2021-03-30 14:16:23 -0400

    wpcs: archive

--- archive.php
+++ archive.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * The template for displaying archive pages.
  *
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
@@ -9,21 +9,25 @@
 
 get_header(); ?>
 
-    <main id="main" role="main">
-      <header class="page-header">
-        <?php
+	<main id="main" role="main">
+		<header class="page-header">
+		<?php
 		the_archive_title( '<h1 class="page-title">', '</h1>' );
 		the_archive_description( '<div class="archive-description">', '</div>' );
 		?>
-      </header><!-- .page-header -->
+		</header><!-- .page-header -->
 
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>
 
 				<?php
-					/* Include the Post-Format-specific template for the content.
+					/*
+					* Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
