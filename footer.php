956bd104b57a02f242628bc9fee642e185042f23
Peter Hebert <peter@peterhebert.com>
2021-06-18 16:50:15 -0400

    fix footer widgets area to 4 columns with clearfix make link icons span screen reader only center calendar widget nav for prev/next month updated compiled css

--- footer.php
+++ footer.php
@@ -9,44 +9,49 @@
 
 $basic_allowed  = simple_grey_basic_allowed_html();
 $footer_bottom  = get_theme_mod( 'simple_grey_footer_text_bottom' );
-$footer_credits = get_theme_mod( 'simple_grey_show_footer_credits' );
+$show_credits = get_theme_mod( 'simple_grey_show_footer_credits' );
 
 ?>
 </div>
 </div>
 </div>
+
 <footer id="footer" class="site-footer" role="contentinfo">
-<div class="wrap">
-<div class="row">
+
 <?php if ( get_theme_mod( 'simple_grey_footer_text_top' ) ) : ?>
-<div class="footer-text"><?php echo esc_html( get_theme_mod( 'simple_grey_footer_text_top' ) ); ?></div>
-<?php endif; ?>
-<?php if ( is_active_sidebar( 'sidebar-footer' ) ) : ?>
-<div class="widget-area">
-	<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'sidebar-footer' ) ) : ?>
-<?php endif; ?>
-</div>
-<!-- .widget-area -->
-<?php endif; ?>
+		<div class="footer-text"><?php echo esc_html( get_theme_mod( 'simple_grey_footer_text_top' ) ); ?></div>
+		<?php endif; ?>
 
-<?php if ( $footer_bottom || 1 === $footer_credits ) : ?>
-	<div class="footer-text">
-			<?php if ( $footer_bottom ) : ?>
-		<p><?php echo wp_kses( $footer_bottom, $basic_allowed ); ?></p>
-<?php endif; ?>
-			<?php if ( 1 === $footer_credits ) : ?>
-				<?php
-				do_action( 'simple_grey_credits' );
-				?>
-	<p><i class="mv mv-wordpress icon-large"></i> <a href="<?php echo esc_url('http://wordpress.org/'); ?>" rel="generator"><?php esc_html_e( 'Proudly powered by WordPress', 'simple-grey' ); ?></a>. <?php esc_html_e( 'Theme', 'simple-grey' ); ?>: <a href="<?php echo esc_url('https://github.com/peterhebert/simple-grey'); ?>"><?php esc_html_e( 'Simple Grey', 'simple-grey' ); ?></a>.
-</p>
+		<?php if ( is_active_sidebar( 'sidebar-footer' ) ) : ?>
+			<div class="widget-area">
+			<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'sidebar-footer' ) ) : ?>
+			<?php endif; ?>
+			</div>
+		<?php endif; ?>
+
+		<?php if ( $footer_bottom ) : ?>
+		<div class="footer-text"><?php echo wp_kses( $footer_bottom, $basic_allowed ); ?></div>
+		<?php endif; ?>
 
 
-<?php endif; ?>
+<?php if ( $show_credits ) : ?>
+	<div class="footer-credits">
+		<p><?php esc_html_e( 'Theme: ', 'simple-grey' ); ?> <a href="<?php echo esc_url('https://wordpress.org/themes/simple-grey/'); ?>">
+		<?php esc_html_e( 'Simple Grey', 'simple-grey' ); ?></a> <br>
+
+		<a href="<?php echo esc_url('https://github.com/peterhebert/simple-grey'); ?>"><?php esc_html_e( 'Theme development on GitHub', 'simple-grey' ); ?></a>
+		
+	</p>
+
+
+	<p><i class="mv mv-wordpress icon-large"></i> <a href="<?php echo esc_url('http://wordpress.org/'); ?>" rel="generator"><?php esc_html_e( 'Proudly powered by WordPress', 'simple-grey' ); ?></a>
+	</p>
+
 </div>
 <?php endif; ?>
-</div>
-</div>
+
+
+
 </footer>
 <!-- #footer -->
 
