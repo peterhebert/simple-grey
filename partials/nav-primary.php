6acf859906df3b71c8c9d0bf888928538edd68ce
Peter Hebert <peter@peterhebert.com>
2026-06-08 18:10:43 -0400

    simplify primary nav partial

--- partials/nav-primary.php
+++ partials/nav-primary.php
@@ -6,28 +6,11 @@
  * @since 1.9.0
  */
 
-// check if accesible menu plugin is active.
-$a11y_nav = is_plugin_active( 'classic-menu-accessible-a11y/classic-menu-accessible-a11y.php' );
-
-if ( has_nav_menu( 'primary' ) ) :
-
-	// load wrappers around menus if using core menus.
-	if ( false === $a11y_nav ) : ?>
-		<div id="navigation">
-			<div class="wrap">
-				<nav class="row" aria-label="<?php esc_attr_e( 'Primary navigation', 'simple-grey' ); ?>">
-		<?php
-	endif;
-
-	simple_grey_main_menu();
-
-	if ( false === $a11y_nav ) :
-		?>
-		</nav>
+if ( has_nav_menu( 'primary' ) ) : ?>
+<div id="navigation">
+	<div class="wrap">
+		<?php simple_grey_main_menu(); ?>
 	</div>
 </div>
-		<?php
-	endif;
-
+	<?php
 endif;
-
